7cac8a33c1ee7fd44c67f1655f6b0ea80d2f3f94
Jonathan Davies <1707239@student.uwtsd.ac.uk>
2019-11-15 16:52:35 +0000

    Defined FileReader functionality.

--- MajorProjectPre/FileReader.cpp
+++ MajorProjectPre/FileReader.cpp
@@ -1,1 +1,37 @@
 #include "FileReader.h"
+
+#pragma region Constructors
+
+FileReader::FileReader(std::string fileName) : fileName(fileName)
+{
+	// Ensure that the file has opened correctly on object creation
+	ErrorCheck();
+}
+
+#pragma endregion
+
+#pragma region Properties
+
+// Return the address of the file
+std::ifstream* FileReader::GetFile() { return &inputFile; }
+
+#pragma endregion
+
+#pragma region Methods
+
+void FileReader::ErrorCheck()
+{
+	// Open the file in read mode
+	inputFile.open(fileName, std::ios::in);
+
+	if(inputFile.is_open())
+	{
+		std::cout << fileName << " : opened successfully." << std::endl;
+	}
+	else
+	{
+		std::cout << fileName << " : cannot be opened." << std::endl;
+	}
+}
+
+#pragma endregion
--- MajorProjectPre/FileReader.h
+++ MajorProjectPre/FileReader.h
@@ -1,4 +1,43 @@
 #pragma once
+
+#pragma region Includes
+
+#include <iostream>
+#include <fstream>
+
+#pragma endregion
+
+
 class FileReader
-{};
+{
+#pragma region Attributes
+
+private:
+	std::ifstream inputFile;
+
+	std::string fileName;
+
+#pragma endregion
+
+#pragma region Constructors
+
+public:
+	FileReader(std::string fileName);
+
+#pragma endregion
+
+#pragma region Properties
+
+public:
+	std::ifstream* GetFile();
+
+#pragma endregion
+
+#pragma region Methods
+
+public:
+	void ErrorCheck();
+
+#pragma endregion
+};
 
