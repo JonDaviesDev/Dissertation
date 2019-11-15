6d61185f19666e66ebbb140397dff9d213ac5624
Jonathan Davies <1707239@student.uwtsd.ac.uk>
2019-11-15 17:11:08 +0000

    Defined BMPFileHeader Functionality.

--- MajorProjectPre/BMPFileHeader.cpp
+++ MajorProjectPre/BMPFileHeader.cpp
@@ -1,1 +1,46 @@
 #include "BMPFileHeader.h"
+
+#pragma region Constructors
+
+BMPFileHeader::BMPFileHeader(BMPInfoHeader* infoHeader)
+	: fileSize(), fileHeaderSize(13)
+{
+	//make getters and setters fr the bmpinfoheader to get access to its attributes here
+
+	fileHeaderSize
+
+		//fileHeaderSize + infoHeaderSize + (bytesPerPixel * width + paddingSize) * height
+
+		CreateFileHeader();
+}
+
+#pragma endregion
+
+#pragma region Properties
+
+short int BMPFileHeader::GetSize() { return fileHeader.size(); }
+
+#pragma endregion
+
+#pragma region Methods
+
+void BMPFileHeader::CreateFileHeader(short int infoHeaderSize)
+{
+	/*
+		fileHeader[0]-[1]	 = file signature (identifies file as a BMP)
+		fileHeader[2]-[5]	 = size of file (bytes)
+		fileHeader[6]-[9]	 = reserved
+		fileHeader[10]-[13]  = starting position of pixel memory
+	*/
+
+
+	fileHeader[0] = (unsigned char)('B');								// Signature part 1
+	fileHeader[1] = (unsigned char)('M');								// Signature part 2
+	fileHeader[2] = (unsigned char)(fileSize);							// FileSize part 1
+	fileHeader[3] = (unsigned char)(fileSize >> 8);						// FileSize part 2
+	fileHeader[4] = (unsigned char)(fileSize >> 16);					// FileSize part 3
+	fileHeader[5] = (unsigned char)(fileSize >> 24);					// FileSize part 4
+	fileHeader[10] = (unsigned char)(fileHeaderSize + infoHeaderSize);	// Size of the header and info
+}
+
+#pragma endregion
--- MajorProjectPre/BMPFileHeader.h
+++ MajorProjectPre/BMPFileHeader.h
@@ -1,4 +1,44 @@
 #pragma once
+
+#pragma region Includes
+
+#include <array>
+#include "BMPInfoHeader.h"
+
+#pragma endregion
+
 class BMPFileHeader
-{};
+{
+#pragma region Attributes
+
+private:
+	const short int fileHeaderSize;
+
+	short int fileSize;
+
+	std::array<unsigned char, 13> fileHeader;
+
+#pragma endregion
+
+#pragma region Constructors
+
+public:
+	BMPFileHeader(BMPInfoHeader* infoHeader);
+
+#pragma endregion
+
+#pragma region Properties
+
+public:
+	short int GetSize();
+
+#pragma endregion
+
+#pragma region Methods
+
+public:
+	void CreateFileHeader(short int infoHeaderSize);
+
+#pragma endregion
+};
 
