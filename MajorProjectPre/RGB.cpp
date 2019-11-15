d719a4945a40c07f1665fece29b4e3ca870ae2c2
Jonathan Davies <1707239@student.uwtsd.ac.uk>
2019-11-15 16:48:44 +0000

    Defined RGB functionality.

--- MajorProjectPre/RGB.cpp
+++ MajorProjectPre/RGB.cpp
@@ -1,1 +1,28 @@
 #include "RGB.h"
+
+#pragma region Constructors
+
+RGB::RGB() : r(0), g(0), b(0) {}
+
+RGB::RGB(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+
+#pragma endregion
+
+#pragma region Properties
+
+// Set & Get Red
+void RGB::SetRed(unsigned char value) { r = value; }
+
+unsigned char RGB::GetRed() { return r; }
+
+// Set & Get Green
+void RGB::SetGreen(unsigned char value) { g = value; }
+
+unsigned char RGB::GetGreen() { return g; }
+
+// Set & GetBlue
+void RGB::SetBlue(unsigned char value) { b = value; }
+
+unsigned char RGB::GetBlue() { return b; }
+
+#pragma endregion
--- MajorProjectPre/RGB.h
+++ MajorProjectPre/RGB.h
@@ -1,4 +1,44 @@
 #pragma once
 class RGB
-{};
+{
+#pragma region Attributes
+
+private:
+	unsigned char r, g, b;
+
+#pragma endregion
+
+#pragma region Constructors
+
+public:
+	RGB();
+
+	RGB(unsigned char r, unsigned char g, unsigned char b);
+
+#pragma endregion
+
+#pragma region Properties
+
+public:
+
+	void SetRed(unsigned char value);
+
+	unsigned char GetRed();
+
+	void SetGreen(unsigned char value);
+
+	unsigned char GetGreen();
+
+	void SetBlue(unsigned char value);
+
+	unsigned char GetBlue();
+
+#pragma endregion
+
+#pragma region Methods
+
+public:
+
+#pragma endregion
+};
 
