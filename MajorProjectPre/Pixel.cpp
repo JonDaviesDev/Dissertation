b3bfcf433e334a3a6fc0b0c4e895a037a8e99f8d
Jonathan Davies <1707239@student.uwtsd.ac.uk>
2019-11-15 17:01:47 +0000

    Defined Pixel functionality

--- MajorProjectPre/Pixel.cpp
+++ MajorProjectPre/Pixel.cpp
@@ -1,1 +1,18 @@
 #include "Pixel.h"
+
+#pragma region Constructors
+
+Pixel::Pixel() : colour(0, 0, 0) {}
+
+Pixel::Pixel(RGB value) : colour(value) {}
+
+#pragma endregion
+
+#pragma region Properties
+
+void Pixel::SetRGB(RGB value) { colour = value; }
+
+RGB Pixel::GetRGB() { return colour; }
+
+#pragma endregion
+
--- MajorProjectPre/Pixel.h
+++ MajorProjectPre/Pixel.h
@@ -1,4 +1,38 @@
 #pragma once
+
+#pragma region Includes
+
+#include "RGB.h"
+
+#pragma endregion
+
+
 class Pixel
-{};
+{
+#pragma region Attributes
+
+private:
+	RGB colour;
+
+#pragma endregion
+
+#pragma region Constructors
+
+public:
+	Pixel();
+
+	Pixel(RGB value);
+
+#pragma endregion
+
+#pragma region Properties
+
+public:
+
+	void SetRGB(RGB value);
+
+	RGB GetRGB();
+
+#pragma endregion
+};
 
